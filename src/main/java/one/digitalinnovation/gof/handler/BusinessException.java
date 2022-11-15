10221f00b28f1cc476f2482f19b330319b3eb47f
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-11-15 20:40:33 -0300

    Add handler errors exceptions

---
+++ src/main/java/one/digitalinnovation/gof/handler/BusinessException.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.gof.handler;
+
+public class BusinessException extends RuntimeException {
+  public BusinessException(String message) {
+    super(message);
+  }
+
+  public BusinessException(String message, Object... params) {
+    super(String.format(message, params));
+  }
+}
